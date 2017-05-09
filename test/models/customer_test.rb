22c53d17b51aa6177c59d2a383808a0a388a8b84
Cara Comfort <cara.comfort10@gmail.com>
2017-05-09 16:35:35 -0700

    Write test for customer relations

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  # let(:customer) { Customer.new }
+  #
+  # it "must be valid" do
+  #   value(customer).must_be :valid?
+  # end
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe "relations" do
+    it "has a list of rentals" do
+      stout = customers(:stout)
+      stout.must_respond_to :rentals
+      stout.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+      end
+    end
+
+    it "has a list of movies" do
+      stout = customers(:stout)
+      stout.must_respond_to :movies
+      stout.movies.each do |movie|
+        movie.must_be_kind_of Movie
+      end
+    end
   end
 end
