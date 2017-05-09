283e5e6d23393c7daa5db5ca12a966974e0c4f63
Allison Northrop <agnorthrop@gmail.com>
2017-05-09 15:40:07 -0700

    added validation for customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,9 @@
 class Customer < ApplicationRecord
   has_many :rentals
-  has_many :movies, through: :rentals 
+  has_many :movies, through: :rentals
+
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :postal_code, length: { is: 5 }
+
 end
