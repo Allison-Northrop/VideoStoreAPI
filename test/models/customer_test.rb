95b64f19325ac6e449ae76a2585178783ad791de
Allison Northrop <agnorthrop@gmail.com>
2017-05-09 16:40:53 -0700

    all customer validation tests passing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -16,5 +16,24 @@ describe "Customer" do
       customer.errors.messages.must_include :phone
     end
 
+    it "requires a postal code" do
+      customer = Customer.create
+      result = customer.valid?
+      result.must_equal false
+      customer.errors.messages.must_include :postal_code
+    end
+
+    it "checks the length isn't greater than 5 for the postal code" do
+      customer = Customer.create(postal_code: "9832928938")
+      result = customer.valid?
+      result.must_equal false
+    end
+
+    it "checks the length of the postal code is not less than 5" do
+      customer = Customer.create(postal_code: "9")
+      result = customer.valid?
+      result.must_equal false
+    end
+
   end
 end
