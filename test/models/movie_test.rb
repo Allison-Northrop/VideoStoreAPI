f47852f8f556cde737e4d748906d0e9b0e4bbe04
Cara Comfort <cara.comfort10@gmail.com>
2017-05-09 16:26:38 -0700

    Write test for movie relations

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  # let(:movie) { Movie.new }
+  #
+  # it "must be valid" do
+  #   value(movie).must_be :valid?
+  # end
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  describe "relations" do
+    it "has a list of customers" do
+      jaws = movies(:jaws)
+      jaws.must_respond_to :customers
+      jaws.customers.each do |customer|
+        customer.must_be_kind_of Customer
+      end
+    end
+
+    it "has a list of rentals" do
+      jaws = movies(:jaws)
+      jaws.must_respond_to :rentals
+      jaws.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+      end
+    end
   end
 end
