7759891d837f0ba72c3cc46cf694034e5145815b
Allison Northrop <agnorthrop@gmail.com>
2017-05-09 15:42:23 -0700

    added rental validations

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,6 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+  #potentially need to comment the below out for a while...
+  validates :due_date, presence: true
 end
