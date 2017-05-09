b20b5c919a08210bbd83434af20726f40086ff92
Cara Comfort <cara.comfort10@gmail.com>
2017-05-09 16:50:18 -0700

    Write tests for movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -24,4 +24,47 @@ describe Movie do
       end
     end
   end
+
+  describe "validations" do
+    it "can be created with all required attributes" do
+      Movie.create!(
+      title: 'test title',
+      inventory: 8
+      )
+    end
+
+    it "requires a title" do
+      movie = Movie.new
+      movie.valid?.must_equal false
+      movie.errors.messages.must_include :title
+    end
+
+    it "requires a unique title" do
+      duplicate_movie = Movie.new(title: "Jaws", inventory: 8)
+      duplicate_movie.valid?.must_equal false
+      duplicate_movie.errors.messages.must_include :title
+    end
+
+    it "requires an inventory" do
+      movie = Movie.new
+      movie.valid?.must_equal false
+      movie.errors.messages.must_include :inventory
+    end
+
+    it "requires the inventory to be an integer" do
+      movie = Movie.new(inventory: "8")
+      movie.valid?.must_equal false
+      movie.errors.messages.must_include :inventory
+    end
+
+    it "requires the inventory to be positive" do
+      movie = Movie.new(inventory: -4)
+      movie.valid?.must_equal false
+      movie.errors.messages.must_include :inventory
+    end
+
+    it "allows an inventory of zero" do
+      Movie.create!(inventory: 0, title: "test title")
+    end
+  end
 end
