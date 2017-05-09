ddf7dbfe18489dc1c333327a516b75ae96d8a360
Cara Comfort <cara.comfort10@gmail.com>
2017-05-09 15:04:03 -0700

    Write validations for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
+
+  validates :title, presence: true
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
---
+++ db/migrate/20170509220431_add_account_credit_column_to_customer.rb
@@ -0,0 +1,5 @@
+class AddAccountCreditColumnToCustomer < ActiveRecord::Migration[5.0]
+  def change
+    add_column :customers, :account_credit, :float
+  end
+end
