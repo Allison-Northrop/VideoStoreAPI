43b9b95ca5549cac7ee58db13dd5a825aed927b7
Allison Northrop <agnorthrop@gmail.com>
2017-05-10 15:17:20 -0700

    wrote an customer index test that passes

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "is a real working route" do
+      get customers_path
+      must_respond_with :success
+    end
+
+  it "returns json" do
+
+  end
+
 end
