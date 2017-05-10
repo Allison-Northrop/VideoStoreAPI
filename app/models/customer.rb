8dac9c0005b5bb0b57a3de444be571cc55dcb02f
Allison Northrop <agnorthrop@gmail.com>
2017-05-10 11:21:57 -0700

    added method in customer model for movie count

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,10 @@ class Customer < ApplicationRecord
   validates :phone, presence: true
   validates :postal_code, length: { is: 5 }
 
+  # scope :movies_checked_out_count, -> { movies.count }
+
+  def movies_checked_out_count
+    movies.count
+  end
+
 end
