388525560bee508d91ab9b8f2c0bb0f92a8ecc7c
Cara Comfort <cara.comfort10@gmail.com>
2017-05-10 12:00:35 -0700

    Add statuses and error messages for movie controller methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,15 @@
 class MoviesController < ApplicationController
   def index
     movies = Movie.all
-    render :json => movies, each_serializer: MoviePreviewSerializer
+    render json: movies, each_serializer: MoviePreviewSerializer, status: :ok
   end
 
   def show
     movie = Movie.find_by(title: params[:title].capitalize)
-    render :json => movie
+    if movie
+      render json: movie, status: :ok
+    else
+      render status: :not_found, json: { errors: { title: ["Movie '#{params[:title]}' not found"] } }
+    end
   end
 end
