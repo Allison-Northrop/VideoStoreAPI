a89db4450be9d0fbf8d8e8f812ffc4fee2d788e6
Allison Northrop <agnorthrop@gmail.com>
2017-05-12 15:40:42 -0700

    wrote some tests for rentals controller

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "checkout" do
+
+    it "a real working route" do
+      customer = customers(:stout)
+      movie = movies(:jaws)
+      post checkout_path(customer_id: customer.id, title: movie.title, due_date: "2017-09-12")
+      must_respond_with :success
+    end
+
+    it "decreases the available inventory by one if the movie is found and the avialable inventory is more than 0" do
+      customer = customers(:stout)
+      movie = movies(:jaws)
+      post checkout_path(customer_id: customer.id, title: movie.title, due_date: "2017-09-12")
+
+      movie.available_inventory.must_equal movie.inventory - 1
+      must_respond_with :success
+    end
+
+    it "renders a bad request status if the available inventory is less than 0" do skip
+      customer = customers(:stout)
+      movie = Movie.new(title: "jaws", overview: "here is an overview", release_date: "1960-06-16", inventory: 0)
+
+        post checkout_path(customer_id: customer.id, title: movie.title, due_date: "2017-09-12")
+        # post checkout_path(customer_id: customer.id, title: movie.title, due_date: "2017-09-11")
+
+      must_respond_with :bad_request
+
+    end
+
+    it "renders a status ok if the rental is succesffully created" do
+    end
+
+    it "renders a bad request if the rental wasn't saved" do skip 
+      # customer = customers(:stout)
+      # post checkout_path(customer_id: customer.id, title: "", due_date: "2017-09-12")
+      Rental.new(customer_id: 1)
+      must_respond_with :bad_request
+    end
+
+  end
 end
