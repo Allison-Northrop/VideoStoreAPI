f42d71e031a7e85b2fe2a985075e4992aae4ad8a
Cara Comfort <cara.comfort10@gmail.com>
2017-05-12 11:11:55 -0700

    Add returned boolean to rental model and set it to false by default

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,4 +3,11 @@ class Rental < ApplicationRecord
   belongs_to :movie
   #potentially need to comment the below out for a while...
   validates :due_date, presence: true
+
+  after_initialize :set_defaults, unless: :persisted?
+
+  private
+  def set_defaults
+    self.returned = false if self.returned.nil?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509220431) do
+ActiveRecord::Schema.define(version: 20170512180314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170509220431) do
     t.date     "due_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "returned"
     t.index ["customer_id"], name: "index_rentals_on_customer_id", using: :btree
     t.index ["movie_id"], name: "index_rentals_on_movie_id", using: :btree
   end
